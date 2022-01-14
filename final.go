137a646a21c2737128b8e85b722c0a536442793d
Arthur Arnx <36545443+Arnouux@users.noreply.github.com>
2022-01-14 13:48:45 +0100

    Update final.go

--- final.go
+++ final.go
@@ -4,9 +4,7 @@ package impl
 
 import (
 	"fmt"
-	"io"
 	"net/http"
-	"strconv"
 	"time"
 )
 
@@ -47,72 +45,4 @@ func (n *Node) SendPrepDwnldResp(dest string, relays map[uint]string) error {
 	//=>implying dynamical size of chunks, to add in route calculationsé & dwnld requests (& type) -> need to decide quickly
 
 	return nil
-}
-
-func getFileSize(url string) (int64, error) {
-	//shamelessy from https://www.socketloop.com/tutorials/golang-get-download-file-size
-
-	resp, err := http.Head(url)
-	if err != nil {
-		fmt.Println(err)
-		return 0, err
-	}
-
-	// Is our request ok?
-
-	if resp.StatusCode != http.StatusOK {
-		fmt.Println(resp.Status)
-		return 0, err
-		// exit if not ok
-	}
-
-	// the Header "Content-Length" will let us know
-	// the total file size to download
-	size, _ := strconv.Atoi(resp.Header.Get("Content-Length"))
-	downloadSize := int64(size)
-	return downloadSize, nil
-
-}
-
-func downloadPartFromExternal(url string, start uint, end uint, IOoutput io.Writer, toStorageCopy io.Writer) error {
-	req, _ := http.NewRequest("GET", url, nil)
-	req.Header.Add("Range", "bytes="+fmt.Sprint(start)+"-"+fmt.Sprint(end))
-	fmt.Println(req)
-	var client http.Client
-	resp, err := client.Do(req)
-
-	if err != nil {
-		println("couldnt do the HTTP request when trying to download/test bdwdth")
-		return err
-	}
-
-	defer resp.Body.Close()
-
-	r := resp.Body
-	r2 := io.TeeReader(r, toStorageCopy)
-	written, err := io.Copy(IOoutput, r2) //check io.PipeWriter to tee data into storage & to requesting node
-	if written != int64(end-start) {
-		println("did Not Download requested amount")
-	}
-	return err
-}
-
-func testExternalBandwidth(url string, upperBound uint) float32 {
-	t0 := time.Now().Unix()
-	downloadPartFromExternal(url, 0, upperBound, io.Discard, io.Discard)
-	t1 := time.Now().Unix()
-	return float32(upperBound) / float32(t0-t1)
-}
-
-func server_to_test(upperBound uint) {
-	http.HandleFunc("/stats", func(w http.ResponseWriter, r *http.Request) {
-		for i := 0; i < int(upperBound); i++ {
-			fmt.Fprintf(w, "A")
-		}
-	})
-
-	if err := http.ListenAndServe("127.0.0.1:8080", nil); err != nil {
-		//we probably already launched it
-		println(err, "while launching server for linkState test")
-	}
 }
